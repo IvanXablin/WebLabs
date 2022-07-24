db1a4ea88922ade45302e3209873e324fb6546b0
Ivan Kondratev <amdandpc67890@gmail.com>
2022-07-25 02:33:03 +0700

    refactor

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 window.onload = function() {
-
+    
     const input = document.querySelector('#in');
     const btn = document.querySelector('#add');
     const result = document.querySelector('#out');
 
     btn.addEventListener('click', (e) => {
-        if (input.value === '') 
-        { return; }
+        if (input.value === '') { return; }
         ManipulateElement(input.value);
     })
 
     
     function ManipulateElement(value) {
+
         const li = document.createElement('li');
         const img = document.createElement('img');
+        const span = document.createElement('span');
 
         li.className = 'list_el';
-        li.textContent = value;     
+        span.textContent = value;  
+ 
         img.className = 'delete';
         img.src = 'img/cross.png';
+
+        li.appendChild(span);
         li.appendChild(img);
 
         img.addEventListener('click', (e) => {  
